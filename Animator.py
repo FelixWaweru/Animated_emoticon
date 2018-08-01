e31d5d0d6b91179b99766b89f496b47738960dca
FelixWaweru <wawerufelix60@gmail.com>
2018-08-01 15:44:13 +0300

    Updates

--- Animator.py
+++ Animator.py
@@ -1,7 +1,14 @@
 import time
+
+charachters = ["( 0|0)", "( o|o)", "( -|-)", "( o|o)", "( 0|0)"]
+eyes = ["0", "o", "-"]
 x = 0
-for x in range (0,5):  
-    b = "Loading" + "." * x
-    print (b, end="\r")
-    x = x+1
-    time.sleep(1)
+for x in range(0, 3):
+    if x > 3:
+        print("( " + eyes[x] + " ͜ʖ " + eyes[x] + ")", end="\r")
+        x = x + 1
+        time.sleep(0.08)
+    else:
+        print("( " + eyes[x] + " ͜ʖ " + eyes[x] + ")", end="\r")
+        x = x - 1
+        time.sleep(0.08)
